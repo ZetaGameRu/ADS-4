85311fdbb3102e8d9d9909ea689c0069f1a36edd
ZetaGameRus <118940070+ZetaGameRu@users.noreply.github.com>
2023-05-09 14:26:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,36 +15,38 @@ int countPairs2(int *arr, int len, int value) {
   int object = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j = len - 1; j > i; j--) {
-      if (arr[i] + arr[j] == value)
+      if (arr[i] + arr[j] == value) {
         object++;
+      }
     }
   }
   return object;
 }
-
 int countPairs3(int *arr, int len, int value) {
   int object = 0;
   for (int i = 0; i < len - 1; i++) {
-    int start = i, stop = len;
-    while (start < stop - 1) {
-      int mid = (start + stop) / 2;
-      if (i != mid && (arr[i] + arr[mid] == value)) {
+    int q = i, z = len;
+    while (1 < z - q) {
+      int qaz = (q + z) / 2;
+      if (arr[i] + arr[qaz] == value) {
         object++;
-        for (int j = mid + 1; (j < stop)
-             && (arr[i] + arr[j] == value)
-             && (mid != i); j++) {
+        int k = qaz + 1;
+        while (arr[i] + arr[k] == value && k < z) {
           object++;
+          k++;
         }
-        for (int j = mid - 1; (j > start)
-             && (arr[i] + arr[j] == value)
-             && (mid != i); j--) {
+        k = qaz - 1;
+        while (arr[i] + arr[k] == value && k > q) {
           object++;
+          k--;
         }
         break;
       }
-      if (arr[i] + arr[mid] > value) stop = mid;
-      else
-        start = mid;
+      if (arr[i] + arr[qaz] > value) {
+        z = qaz;
+      } else {
+        q = qaz;
+      }
     }
   }
   return object;
